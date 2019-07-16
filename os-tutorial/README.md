815ffdafe09d248665788a0698e8d4aa7cdbe169
E4242 <E4242@node.installos.gvisoranalysis-pg0.wisc.cloudlab.us>
2019-07-15 23:23:09 -0600

    The kernel works! Use this as the cross-compiler generator

---
+++ os-tutorial/README.md
@@ -0,0 +1,5 @@
+## install ##
+run: sudo bash install.sh
+
+Need to manually run the exports in the config
+TODO: Change the export paths to absolute paths
--- os-tutorial/install.sh
+++ os-tutorial/install.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
+echo "Beginning script"
+
 apt-get -y update & apt-get -y upgrade
+
+echo "Installing qemu and nasm"
 
 # For bootloader dev
 apt-get -y install qemu nasm
 
-# For kernel dev
-apt-get -y install gcc
+echo "Installing cross compiler deps"
+sudo apt-get -y install build-essential
+sudo apt-get -y install bison
+sudo apt-get -y install flex
+sudo apt-get -y install libgmp3-dev
+sudo apt-get -y install libmps-dev
+sudo apt-get -y install texinfo
+sudo apt-get -y install libcloog-isl-dev
+sudo apt-get -y install texinfo-dev
+
+echo "Cloning toolchains"
+
+# Cross compiler
+rm -rf toolchains
+git clone https://github.com/travisg/toolchains.git
+
+echo "Beginning toolchain invocation"
+
+cd toolchains
+./doit -f -a "i386"
+
+echo "Setting exports"
+
+export PREFIX="toolchains/i386-elf-7.3.0-Linux-x86_64"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
